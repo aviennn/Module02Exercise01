3a5b4e2c92d0e89bd17ade3b4b9a1f6f616d154d
avien <batul.avienflaire@auf.edu.ph>
2024-08-28 13:37:29 +0800

    Add project files.

---
+++ Model/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module02Exercise01.Model
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullNames => $"{FirstName} {LastName}";
+        public string Position { get; set; }
+        public string Department { get; set; }
+
+        public string PosDep => $"{Position}" + $" / " + $"{Department}" + $" Department" + $" / " + $" Status of Employee: " + $"{IsActive}";
+
+        public bool IsActive { get; set; }
+
+    }
+}
